713b26d7320a56ac5121a5ce78f09908af2fe288
zhanlon <zjhanlon@gmail.com>
2018-04-02 11:52:41 -0700

    randomizer

---
+++ randomizer.cpp
@@ -0,0 +1,17 @@
+#include<ctime>
+#include<cstdlib>
+
+
+unsigned int Randomizer:: Randomizer()
+{
+	
+}
+
+
+
+unsigned int Randomizer::randomBetween(unsigned int minimum, unsigned int maximum)
+{
+	
+}
+
+//Im honestly not understanding the problem...
